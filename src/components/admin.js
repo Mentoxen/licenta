86fea7841dfcc69be3d293c86e9dc9ee86d0d91e
Iuliu Dascalu <iuliu.dascalu@pitechnologies.ro>
2020-06-10 02:41:46 +0300

    added admin.

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,10 +1,41 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 
 const Admin = () => {
+  const [tagName, setTagName] = useState({
+
+  })
+
+  const defaultTags = {tag: "tag", name: ""};
+  const [tags, setTags] = useState([defaultTags]);
+
+  const addTag = () => {
+    setTags([...tags, {...defaultTags}]);
+  }
+
+  const handleTagChange = (e) => {
+    const updatedTag = [...tags];
+    updatedTag[e.target.className] = e.target.value;
+    setTags(updatedTag)
+    console.log(updatedTag)
+  }
 
   return(
-      <h1 className="pt-12">This is admin</h1>
+      <section className="inner-page">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              {
+                tags && tags.map((tag, index) => {
+                  return <input className="name" name={`tag-${index}`} key={index} data-index={index} onChange={handleTagChange} />
+                })
+              }
+
+              <button className="btn btn-small btn-secondary" onClick={() => addTag()}>Add tag</button>
+            </div>
+          </div>
+        </div>
+      </section>
   )
 }
 
